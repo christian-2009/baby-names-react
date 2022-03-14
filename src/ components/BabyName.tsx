1af741dcc3f4487419f8abc6c683a9bac2ade0e7
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-14 15:38:13 +0000

    some ideas in babynames

--- src/ components/BabyName.tsx
+++ src/ components/BabyName.tsx
@@ -5,10 +5,18 @@ interface BabyInfo {
 
 
 export default function BabyName(props: BabyInfo): JSX.Element{
-    return(
-        <>
+        return (
+           <>
             {props.sex === 'f' && <p className='female-style'>{props.name}</p>}
-            {props.sex === 'm' && <p className ='male-style'>{props.name}</p>}
-        </>
-    )
+            {props.sex === 'm' && <p className='female-style'>{props.name}</p>}
+           </>
+        )
 }
+
+/* 
+IDEAS
+since we want to change all of the values on the screen, I think we should add some conditionality in this function using the {text} state from 
+SearchBar. 
+If text = '' then display all names 
+Otherwise if props.name.includes({text}) display that and the other names
+*/
