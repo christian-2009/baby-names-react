78d9a56a94b70dfd4cd59242d8dfd4d79a63fc9e
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-16 16:39:58 +0000

    fixed colour error with males

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -8,7 +8,7 @@ export default function BabyName(props: BabyInfo): JSX.Element{
         return (
            <>
             {props.sex === 'f' && <p className='female-style'>{props.name}</p>}
-            {props.sex === 'm' && <p className='female-style'>{props.name}</p>}
+            {props.sex === 'm' && <p className='male-style'>{props.name}</p>}
            </>
         )
 }
