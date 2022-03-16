c5693b80f82dca36edbfb90fca9ae6a13c5a5d90
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-16 17:16:10 +0000

    have created the BabyNames fnction insdie my main and styled the buttons

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,13 +2,24 @@ import BabyName from "./BabyName";
 import { sortedBabyData } from "../utils/sortedBabyData";
 import SearchBar from "./SearchBar";
 
+
+
+
 export default function MainContent(): JSX.Element{
-    const listItems = sortedBabyData.map(BabyName)
+    const babyNames = sortedBabyData.map((babyObj) => {
+    if (babyObj.sex === 'f'){
+        return(
+        <button className = 'female-style' key={babyObj.id}>{babyObj.name}</button>)}
+    else{
+        return (
+        <button className = 'male-style' key={babyObj.id}>{babyObj.name}</button>)
+    }})
     return (
         <>
             <h1 className='title'>Babynames</h1>
             <SearchBar />
-            <p>{listItems}</p>
+            {babyNames}
+            
         </>
     )
 }
